e0232fa4718f10f1e1239b4769ba4a788690cab9
Luis <luisme@luiss-mbp.lan>
2021-07-07 10:20:38 -0400

    add gratuity function

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ function say(something){
     console.log(something);
 }
 
+function gratuity(total){
+    console.log("10%:" + 0.10*total )
+    console.log("15%:" + 0.15*total )
+    console.log("20%" + 0.20*total) 
+}
+
+gratuity(20);
+
 
 
 
