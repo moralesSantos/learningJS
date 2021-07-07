7bfff858b1bad109c9620bc117f9c2ecdcc47ab3
Luis <luisme@luiss-mbp.lan>
2021-07-07 01:41:23 -0400

    add new file index and script

---
+++ script.js
@@ -0,0 +1,49 @@
+/*
+Write a script that:
+Stores the number of cups (that a person has consumed) in a variable
+If the person has had *less than* 3 cups
+Log a message to the console saying: ("Yes I'll take another cup of coffee.")
+If not
+Log a message to the console saying ("I think I'm okay for now.")
+*/
+let numCups = 2; //#number of cups of coffee consumed
+
+if(numCups<3){
+    console.log("Yes I'll take another cup of coffee.");
+} else{
+console.log("I think I'm okay for now.");
+}
+
+
+/*
+Does your car need an oil change?
+Write a script that: Stores the number of miles(that a car has) in a variable
+If the car has *more than* 500 miles
+Log a message to the console saying:("Yes I think I need an oil change.")
+If not
+Log a message to the console saying:("My car doesn't need an oil change yet.")
+*/
+
+let miles = 250; //number of miles a car has
+
+if(miles>500){
+    console.log("Yes I think I need an oil change.");
+}else{
+    console.log("My car doesn't need an oil change yet");
+}
+
+
+let num = 100;
+
+// for (let i = 0; i<100;i++){
+//     if(i%2==0){
+//         console.log(i + " is even")
+//     } else{
+//         console.log(i + " is odd")
+//     }
+// } 
+
+
+
+
+
