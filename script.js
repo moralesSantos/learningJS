5704eae075a2ad03d4cbbfc48338bd3c0451b813
Luis <luisme@luiss-mbp.lan>
2021-07-07 09:54:02 -0400

    add funcions

--- script.js
+++ script.js
@@ -1,48 +1,18 @@
-/*
-Write a script that:
-Stores the number of cups (that a person has consumed) in a variable
-If the person has had *less than* 3 cups
-Log a message to the console saying: ("Yes I'll take another cup of coffee.")
-If not
-Log a message to the console saying ("I think I'm okay for now.")
-*/
-let numCups = 2; //#number of cups of coffee consumed
+console.log("script is loaded");
+// let a =1;
+// let b=2;
 
-if(numCups<3){
-    console.log("Yes I'll take another cup of coffee.");
-} else{
-console.log("I think I'm okay for now.");
+// console.log(a+b);
+
+function add(a,b){
+    return a+b;
 }
 
+console.log(add(5, 6));
 
-/*
-Does your car need an oil change?
-Write a script that: Stores the number of miles(that a car has) in a variable
-If the car has *more than* 500 miles
-Log a message to the console saying:("Yes I think I need an oil change.")
-If not
-Log a message to the console saying:("My car doesn't need an oil change yet.")
-*/
-
-let miles = 250; //number of miles a car has
-
-if(miles>500){
-    console.log("Yes I think I need an oil change.");
-}else{
-    console.log("My car doesn't need an oil change yet");
+function say(something){
+    console.log(something);
 }
-
-
-let num = 100;
-
-// for (let i = 0; i<100;i++){
-//     if(i%2==0){
-//         console.log(i + " is even")
-//     } else{
-//         console.log(i + " is odd")
-//     }
-// } 
-
 
 
 
