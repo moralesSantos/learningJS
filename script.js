ad7b286e74f4581077d38774a1712248a8389251
luis <jmsantos879@gmail.com>
2021-08-02 02:14:23 -0400

    add new function

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 
-let numbers = [1, 2, 4, 7, 3, 5, 6];
+
+
+/**CallBack HW */
+const message = function() {  
+    console.log("This message will show after 4 secs");
+}
+
+setTimeout(message,4000);
+//
+
+
+
+let numbers = [1, 2, 4, 7, 3, 5, 6, 8, 15];
 
 function isOddNumber(number) {
     return number % 2;
@@ -7,3 +19,8 @@ function isOddNumber(number) {
 
 const oddNumbers = numbers.filter(isOddNumber);
 console.log(oddNumbers); // [ 1, 7, 3, 5 ]
+
+// document.queryselector("#callback-btn")
+//     .addEventListener("click", function() {    
+//       console.log("User has clicked on the button!");
+// });
