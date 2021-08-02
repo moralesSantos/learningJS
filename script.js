dc0cf6ac610c4dd6b8f2f2e6220cc5835c385545
luis <jmsantos879@gmail.com>
2021-08-02 01:58:53 -0400

    callback function

--- script.js
+++ script.js
@@ -1,69 +1,9 @@
-console.log("script is loaded");
-// let a =1;
-// let b=2;
 
-// console.log(a+b);
+let numbers = [1, 2, 4, 7, 3, 5, 6];
 
-function add(a,b){
-    return a+b;
+function isOddNumber(number) {
+    return number % 2;
 }
 
-console.log(add(5, 6));
-
-function say(something){
-    console.log(something);
-}
-
-function gratuity(total){
-    console.log("10%:" + 0.10*total )
-    console.log("15%:" + 0.15*total )
-    console.log("20%" + 0.20*total) 
-}
-
-gratuity(20);
-
-let a = "blank";
-
-if (0>1){
-    a = "hello";
-    console.log(a);
-}
-
-console.log(a);
-
-
-(function myFunction(word){
-    console.log("oooo so anonymous! The word is: " + word);
-})("bird");
-
-// myFunction("woah");
-
-
-//Declare an add function
-function add(number1, number2) {
-	return number1 + number2;
-}
-
-//Declare a function that takes a function as an argument
-function doMath(operation, number1, number2) {
-	return operation(number1,number2);
-}
-
-
-//Pass a function into another.
-let sum = doMath(add, 1, 2);
-
-console.log(sum);
-
-let students = [
-    {name: 'Max Chewnning', average:90},
-    {name: 'Christian Guzman', average:65},
-    {name: 'Travis Scott', average:82}
-];
-
- 
-let passingStudents = students.filter(function(student){
-    return student.average >70;
-});
-
-console.log(passingStudents[0].name);
+const oddNumbers = numbers.filter(isOddNumber);
+console.log(oddNumbers); // [ 1, 7, 3, 5 ]
